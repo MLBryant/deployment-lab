18184391a0b8f130ed4759215a4bda768c71f6e5
Mason Bryant <mlbryant6@gmail.com>
2021-11-17 14:33:46 -0600

    css test

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ const app = express()
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'))
 })
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/style.css'))
+})
 
 const server = process.env.PORT || 4000
 app.listen(server, () => console.log(`Server is running on ${server}`))
