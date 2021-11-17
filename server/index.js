75c6b52b88ec00d9a6c2493a189de435d391736f
Mason Bryant <mlbryant6@gmail.com>
2021-11-17 14:12:20 -0600

    first commit

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'))
+})
+
+const server = process.env.PORT || 4000
+app.listen(server, () => console.log(`Server is running on ${server}`))
