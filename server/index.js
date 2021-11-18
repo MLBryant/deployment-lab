522c221e598dfd8ff4b52e33c988096d61bd020d
Mason Bryant <mlbryant6@gmail.com>
2021-11-18 17:05:27 -0600

    bleh

--- server/index.js
+++ server/index.js
@@ -3,15 +3,6 @@ const path = require('path')
 const app = express()
 
 // include and initialize the rollbar library with your access token
-var Rollbar = require('rollbar')
-var rollbar = new Rollbar({
-  accessToken: '2095f7101147464eb8a5fe1ec04c066c',
-  captureUncaught: true,
-  captureUnhandledRejections: true,
-})
-
-// record a generic message and send it to Rollbar
-rollbar.log('Hello world!')
 
 
 app.use('/css', express.static(path.join(__dirname, 'client/style.css')))
@@ -22,6 +13,16 @@ app.get('/', (req, res) => {
 app.get('/css', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/style.css'))
 })
+
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '2095f7101147464eb8a5fe1ec04c066c',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
 
 app.get('/morecats', (req, res) => {
     getAllTheCats()
