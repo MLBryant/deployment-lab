497a9a61398b40360cf69b194d36d0e1841cea61
Mason Bryant <mlbryant6@gmail.com>
2021-11-18 16:22:53 -0600

    trying rollbar features

--- server/index.js
+++ server/index.js
@@ -2,6 +2,18 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '2095f7101147464eb8a5fe1ec04c066c',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+
 app.use('/css', express.static(path.join(__dirname, 'client/style.css')))
 
 app.get('/', (req, res) => {
@@ -9,6 +21,10 @@ app.get('/', (req, res) => {
 })
 app.get('/css', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/style.css'))
+})
+
+app.get('/morecats', (req, res) => {
+    getAllTheCats()
 })
 
 const server = process.env.PORT || 4000
